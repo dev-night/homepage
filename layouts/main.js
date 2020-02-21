90b6a64119edbd7860c5810c5c6e4d61890c25c8
Niklas Heer <niklas.heer@tradebyte.com>
2020-02-21 10:19:01 +0100

    :arrow_up: Update sharp

--- layouts/main.js
+++ layouts/main.js
@@ -1,6 +1,5 @@
 import Header from "../components/header";
 import Nav from "../components/nav";
-import LiveStream from "../components/livestream";
 import Footer from "../components/footer";
 import firebase from "../lib/firebase";
 
@@ -9,7 +8,6 @@ export default ({ children }) => (
     <Header />
     <div className="container">
       <Nav />
-      <LiveStream />
       {children}
       <Footer />
     </div>
